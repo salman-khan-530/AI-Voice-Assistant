ebc9df5f7e8d9002a904a2758e69639e3984cadd
salman-khan-530 <salman22000055@gmail.com>
2026-08-25 14:57:26 +0500

    Update TTS for Streamlit deployment

--- src/tts.py
+++ src/tts.py
@@ -1,27 +1,33 @@
-import pyttsx3
+from groq import Groq
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+
+# Create Groq client
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
 
 
 def text_to_speech(text, output_file="response.wav"):
+    """
+    Convert text into speech using Groq TTS.
+    """
+
     try:
-        engine = pyttsx3.init()
-        engine.save_to_file(text, output_file)
-        engine.runAndWait()
-        engine.stop()
+        response = client.audio.speech.create(
+            model="canopylabs/orpheus-v1-english",
+            voice="troy",
+            input=text,
+            response_format="wav"
+        )
+
+        response.write_to_file(output_file)
+
         return output_file
 
-        
     except Exception as e:
-        print(f"Text-to-speech error: {e}")
+        print(f"TTS Error: {e}")
         return None
-
-if __name__ == "__main__":
-
-    text = "Hello! I am your AI voice assistant."
-
-    audio_file = text_to_speech(
-        text,
-        "response.wav"
-    )
-
-    print("Audio file:", audio_file)
